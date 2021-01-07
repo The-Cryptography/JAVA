7b795d1ccdb24c91fb6bd47b5b6f12eadf97ba5e
Anviksha Jain <56529970+Anvikshajain@users.noreply.github.com>
2021-01-07 09:58:59 +0530

    Update RSA_algorithm.java

--- ciphers/RSA_algorithm.java
+++ ciphers/RSA_algorithm.java
@@ -1,4 +1,5 @@
 /*
+* RSA Algorithm
 * RSA is one of the first practicable public-key cryptosystems and is widely used for secure data transmission.
 * In such a cryptosystem, the encryption key is public and differs from the decryption key which is kept secret. 
 * In RSA, this asymmetry is based on the practical difficulty of factoring the product of two large prime numbers, the factoring problem.
