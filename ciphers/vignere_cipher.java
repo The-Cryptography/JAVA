8c82d26d508cbdb8885d15a6069745f3446c03a6
Anviksha Jain <56529970+Anvikshajain@users.noreply.github.com>
2021-01-07 09:59:51 +0530

    Update vignere_cipher.java

--- ciphers/vignere_cipher.java
+++ ciphers/vignere_cipher.java
@@ -1,4 +1,5 @@
 /*
+* Vigenere Cipher
 * Vigenere Cipher is a method of encrypting alphabetic text.
 * It uses a simple form of polyalphabetic substitution. A polyalphabetic cipher 
 * is any cipher based on substitution, using multiple substitution alphabets 
